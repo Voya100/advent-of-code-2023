6a60c1f5cfa1bc20565440901b0b186153b16db0
Voya <voya@voyacode.com>
2023-12-01 18:21:24 +0200

    Refactor day 1 solution

--- src/day1.ts
+++ src/day1.ts
@@ -2,7 +2,7 @@
 
 import { sum } from './utils';
 
-const numberMap = {
+const numberMap: Record<string, number> = {
   1: 1,
   2: 2,
   3: 3,
@@ -23,6 +23,9 @@ const numberMap = {
   nine: 9,
 };
 
+const firstDigitRegex = new RegExp(Object.keys(numberMap).join('|'));
+const lastDigitRegex = new RegExp(reverseString(Object.keys(numberMap).join('|')));
+
 export function part1(input: string) {
   const values = parseInput(input);
   return sum(values, (v) => v);
@@ -41,30 +44,26 @@ function parseInput2(input: string) {
   return input.split('\n').map(findNumber2);
 }
 
+/**
+ * Find first and last digit in string, concatenate them and return as number
+ */
 function findNumber(row: string) {
   const digitMatches = [...row.matchAll(/\d/g)];
-  const numberString =
-    digitMatches[0][0] + digitMatches[digitMatches.length - 1][0];
+  const numberString = digitMatches[0][0] + digitMatches[digitMatches.length - 1][0];
   return +numberString;
 }
 
+/**
+ * Find first and last representations of digit in string ("1", "one", "2", ...), concatenate them and return as number
+ */
 function findNumber2(row: string) {
-  let firstIndex = row.length;
-  let firstValue = 0;
-  let lastIndex = -1;
-  let lastValue = 0;
-  for (const [key, value] of Object.entries(numberMap)) {
-    const firstIndexOfKey = row.indexOf(key);
-    const lastIndexOfKey = row.lastIndexOf(key);
-    if (firstIndexOfKey !== -1 && firstIndexOfKey < firstIndex) {
-      firstIndex = firstIndexOfKey;
-      firstValue = value;
-    }
-    if (lastIndexOfKey !== -1 && lastIndexOfKey > lastIndex) {
-      lastIndex = lastIndexOfKey;
-      lastValue = value;
-    }
-  }
-  const numberString = firstValue + '' + lastValue;
+  const reversedRow = reverseString(row);
+  const firstDigitKey = row.match(firstDigitRegex)![0];
+  const lastDigitKey = reverseString(reversedRow.match(lastDigitRegex)![0]);
+  const numberString = `${numberMap[firstDigitKey]}${numberMap[lastDigitKey]}`;
   return +numberString;
+}
+
+function reverseString(str: string) {
+  return str.split('').reverse().join('');
 }
