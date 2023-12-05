b13fde4db284263a42cd4404f16bd74bcbd20376
Voya <voya@voyacode.com>
2023-12-05 17:48:34 +0200

    Optimise day 5 solution

--- src/day5.ts
+++ src/day5.ts
@@ -2,14 +2,17 @@
 
 import { toRecord } from './utils';
 
+type Range = {
+  start: number;
+  end: number;
+};
+
 type SourceMap = {
   sourceCategory: string;
   destinationCategory: string;
   ranges: {
-    destinationRangeStart: number;
-    destinationRangeEnd: number;
-    sourceRangeStart: number;
-    sourceRangeEnd: number;
+    sourceRange: Range;
+    destinationRange: Range;
   }[];
 };
 
@@ -20,17 +23,18 @@ export function part1(input: string) {
   return Math.min(...locations);
 }
 
-// Brute force solution. Takes ~21 minutes to run
 export function part2(input: string) {
   const { seeds, maps } = parseInput(input);
   const mapsBySource = toRecord(maps, (map) => map.sourceCategory);
   let minLoc = Infinity;
   for (let i = 0; i < seeds.length - 1; i += 2) {
-    for (let j = seeds[i]; j < seeds[i] + seeds[i + 1]; j++) {
-      const location = findLocation(j, 'seed', mapsBySource);
-      if (location < minLoc) {
-        minLoc = location;
-      }
+    const location = findLowestLocationForRange(
+      { start: seeds[i], end: seeds[i] + seeds[i + 1] - 1 },
+      'seed',
+      mapsBySource
+    );
+    if (location < minLoc) {
+      minLoc = location;
     }
   }
   return minLoc;
@@ -51,16 +55,22 @@ function parseMap(mapInput: string): SourceMap {
   // Format "seed-to-soil map:"
   const [sourceCategory, destinationCategory] = rows[0].split(' ')[0].split('-to-');
   const ranges = rows.slice(1).map(parseRange);
+  // Sort by ascending start order so that gaps between ranges can be found more easily
+  ranges.sort((r1, r2) => r1.sourceRange.start - r2.sourceRange.start);
   return { sourceCategory, destinationCategory, ranges };
 }
 
 function parseRange(row: string) {
   const [destinationRangeStart, sourceRangeStart, rangeLength] = row.split(' ').map((v) => +v);
   return {
-    destinationRangeStart,
-    destinationRangeEnd: destinationRangeStart + rangeLength,
-    sourceRangeStart,
-    sourceRangeEnd: sourceRangeStart + rangeLength,
+    destinationRange: {
+      start: destinationRangeStart,
+      end: destinationRangeStart + rangeLength,
+    },
+    sourceRange: {
+      start: sourceRangeStart,
+      end: sourceRangeStart + rangeLength,
+    },
   };
 }
 
@@ -70,10 +80,81 @@ function findLocation(id: number, source: string, mapsBySource: Record<string, S
   }
   const map = mapsBySource[source];
   for (const range of map.ranges) {
-    if (range.sourceRangeStart <= id && id < range.sourceRangeEnd) {
-      const diff = id - range.sourceRangeStart;
-      return findLocation(range.destinationRangeStart + diff, map.destinationCategory, mapsBySource);
+    if (range.sourceRange.start <= id && id < range.sourceRange.end) {
+      const diff = id - range.sourceRange.start;
+      return findLocation(range.destinationRange.start + diff, map.destinationCategory, mapsBySource);
     }
   }
   return findLocation(id, map.destinationCategory, mapsBySource);
+}
+
+/**
+ * Finds smallest location value for given idRange of given source type.
+ */
+function findLowestLocationForRange(idRange: Range, source: string, mapsBySource: Record<string, SourceMap>) {
+  const map = mapsBySource[source];
+  if (source === 'location') {
+    return idRange.start;
+  }
+  // Note: These are ordered in ascending order
+  const sourceRanges: Range[] = map.ranges.map((r) => r.sourceRange);
+  const lastSourceRange = sourceRanges[sourceRanges.length - 1];
+  // Ranges within idRange not contained within sourceRanges
+  const outsideRanges: Range[] = [];
+  const lowestLocations: number[] = [];
+
+  // idRange section on left side of first sourceRange, if exists
+  const firstOutsideRange = getOverlap(idRange, { start: 0, end: sourceRanges[0].start });
+  // idRange section on right side of last sourceRange, if exists
+  const lastOutsideRange = getOverlap(idRange, { start: lastSourceRange.end, end: Infinity });
+  if (firstOutsideRange) {
+    outsideRanges.push(firstOutsideRange);
+  }
+  if (lastOutsideRange) {
+    outsideRanges.push(lastOutsideRange);
+  }
+
+  let previousRange: Range | null = null;
+  for (const range of map.ranges) {
+    const { sourceRange, destinationRange } = range;
+    if (previousRange !== null && previousRange.end !== sourceRange.start) {
+      // There is empty space between sourceRanges. Check if idRange overlaps with this section
+      const overLap = getOverlap(idRange, { start: previousRange.end, end: sourceRange.start });
+      if (overLap) {
+        outsideRanges.push(overLap);
+      }
+    }
+    const overlapWithSourceRange = getOverlap(idRange, sourceRange);
+    if (overlapWithSourceRange) {
+      const overlapLength = overlapWithSourceRange.end - overlapWithSourceRange.start;
+      const diff = overlapWithSourceRange.start - sourceRange.start;
+      const mappedOverlap = {
+        start: destinationRange.start + diff,
+        end: destinationRange.start + diff + overlapLength + 1,
+      };
+      lowestLocations.push(findLowestLocationForRange(mappedOverlap, map.destinationCategory, mapsBySource));
+    }
+    previousRange = sourceRange;
+  }
+
+  for (const range of outsideRanges) {
+    // Ids outside outside of source ranges don¨t need any mapping
+    lowestLocations.push(findLowestLocationForRange(range, map.destinationCategory, mapsBySource));
+  }
+
+  return Math.min(...lowestLocations);
+}
+
+/**
+ * Finds overlap between 2 ranges. Retuns null if no overlap found
+ */
+function getOverlap(range1: Range, range2: Range): Range | null {
+  const possibleOverlap = {
+    start: Math.max(range1.start, range2.start),
+    end: Math.min(range1.end, range2.end),
+  };
+  if (possibleOverlap.start < possibleOverlap.end) {
+    return possibleOverlap;
+  }
+  return null;
 }
