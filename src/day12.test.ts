fe3cc07c55d7e9e46c8fa9b3e4a478b960e05480
Voya <voya@voyacode.com>
2023-12-12 21:58:23 +0200

    Remove commented out code

--- src/day12.test.ts
+++ src/day12.test.ts
@@ -10,14 +10,11 @@ const input = `???.### 1,1,3
 describe('day 12, part 1', () => {
   it('should work with test input', () => {
     expect(part1(input)).toBe(21);
-    //expect(part1('?#.???#???????##?.? 2,1,7,3,1')).toBe(2);
-    // ##.???#???????##?.# 1,7,3
   });
 
   it('should work with more inputs', () => {
     expect(part1('?..#..### 1,3')).toBe(1);
     expect(part1('...?????.?? 1,2,1')).toBe(6);
-    // ...?? ?? ? 1,2
   });
 });
 
